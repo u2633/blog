6efdd547b72700598a22bb2556202849228906da
Zane Chen <zhenyang0703@gmail.com>
2022-08-14 17:48:04 +0800

    add shell script for post creating

--- create_post.sh
+++ create_post.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-hugo new  --kind post zh-hant/post/my-article-name
+if [ -z "$1" ]
+  then
+    echo "Usage: ./create_post.sh my-post"
+    exit -1
+fi
+
+NAME=$1
+YEAR=$(date +"%Y")
+MONTH=$(date +%m)
+
+hugo new --kind post post/${YEAR}/${MONTH}/${NAME}
